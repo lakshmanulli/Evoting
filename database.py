cde27e1efd67cbc269c1c40b2cf733a78130bf82
lakshman.ulli22@gmail.com <lakshman.ulli22@gmail.com>
2024-06-05 11:49:47 +0530

    evoting

---
+++ database.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evoting',
+        'USER': 'root',
+        'PASSWORD': 'YES',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
